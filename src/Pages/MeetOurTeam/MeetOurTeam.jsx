050fd9f890e3f570ace2706cca32f758af91e1c3
Vishnu teja <vishnuteja476@gmail.com>
2026-05-22 12:02:40 +0530

    added notice about rishab

--- src/Pages/MeetOurTeam/MeetOurTeam.jsx
+++ src/Pages/MeetOurTeam/MeetOurTeam.jsx
@@ -883,7 +883,16 @@ export default function MeetOurTeam() {
             </div>
           </section>
           <div className={Style.noteContainer}>
-            <p>Note : Rishab is no longer associated with Fracspace</p>
+            <p>
+              <span className={Style.impInfo}>Important Customer Advisory:</span> Mr. Rishab Tirunahari,
+              Former Director of Sales, is no longer associated with Fracspace
+              and is not authorised to represent, communicate, collect payments,
+              make commitments, or act on behalf of Fracspace Private Limited or
+              its group entities in any manner. Customers and investors are
+              requested to verify all communication only through official
+              Fracspace channels. We wish him all the best in his future
+              endeavors.
+            </p>
           </div>
         </div>
 
